98a70ef07d9f32cd633c35db60df749a2de9485b
prottoy6218 <prottoy1862@gmail.com>
2022-11-13 01:46:12 +0600

    arithmetic

---
+++ rectangle.js
@@ -0,0 +1,10 @@
+// Write a program to input length and width of a rectangle and calculate perimeter of the rectangle. 
+
+// Example
+// Input
+// Enter length: 5
+// Enter width: 10
+// Output
+
+// Perimeter of rectangle = 30 units
+
