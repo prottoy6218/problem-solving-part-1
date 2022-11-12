b6f22c30ddbed1acd99ee677892943dfb4a5e143
prottoy6218 <prottoy1862@gmail.com>
2022-11-13 01:05:53 +0600

    ques added

--- add-two-number.js
+++ add-two-number.js
@@ -1,3 +1,11 @@
+// Write a C program to input two numbers from user and calculate their sum.
+// Example
+// Input
+// Input first number: 20
+// Input second number: 10
+// Output
+// Sum = 30
+
 function addNumbers(num1, num2){
     const sum = (num1 + num2);
     return ('Sum = ' + sum);
