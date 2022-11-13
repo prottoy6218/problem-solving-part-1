4023ef9c307d12e89c0443a30d56b153dc5daca9
prottoy6218 <prottoy1862@gmail.com>
2022-11-13 12:21:43 +0600

    find square root of a number

---
+++ squareRoot.js
@@ -0,0 +1,14 @@
+// Write a program to input a number and find square root of the given number. 
+// Example
+// Input
+// Enter any number: 9
+// Output
+
+// Square root of 9 = 3
+
+function inputNumber(num){
+    console.log(Math.sqrt(num));
+}
+
+inputNumber(9);
+inputNumber(12);
