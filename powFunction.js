deba55e02a14b0021f5836bf8abd114779a0b4c7
prottoy6218 <prottoy1862@gmail.com>
2022-11-13 11:44:28 +0600

    power of a number using pow function

---
+++ powFunction.js
@@ -0,0 +1,16 @@
+// Write a program to input two numbers from user and find their power using pow() function. 
+
+// Example
+// Input
+// Enter base: 5
+// Enter exponent: 2
+// Output
+
+// 5 ^ 2 = 25
+
+function enterNumber(base, exponent){
+    console.log(Math.pow(base, exponent));
+}
+
+enterNumber(5, 2);
+enterNumber(5, 3);
